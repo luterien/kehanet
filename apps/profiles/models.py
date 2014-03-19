fd646608f6765f31794ae7d4f60a78e846e6cb7f
root <root@guest-4>
2014-03-19 11:00:23 +0100

    fixed windows pillow issue

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -8,6 +8,9 @@ class Profile(AbstractUser):
     birthdate = models.DateTimeField(_("Birth Date"),
                                      null=True, blank=True)
 
+    picture = models.ImageField(_("Profile Picture"),
+                                upload_to="users/avatars/",
+                                null=True, blank=True)
 
     about = models.CharField(_(u"About"), max_length=300, null=True, blank=True)
 
